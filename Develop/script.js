02742710325058bd0494bfe5316bcf32672fde06
Jbolts <77908382+Jbolton89@users.noreply.github.com>
2021-04-02 17:45:46 +1100

    second commit

--- Develop/script.js
+++ Develop/script.js
@@ -6,21 +6,42 @@ var rightNow = function () {
 }
 setInterval(rightNow, 1000);
 
-// For sorting the rows
-$(function () {
-    $("#sortable").children(".textarea").sortable(
-        placeholder,'ui-state-highlight',
-    )   
-    },
-)
-// why doesnt this work? Now the whole area doesnt work
+// For sorting the rows if you want to drag meetings around
+    // $("#sortable").children(".textarea").sortable({
+    //     placeholder:'ui-state-highlight', 
+    // });
 
-$(".savebtn").on('click', function () {
-    console.log(this);
+$(".saveBtn").on('click', function () {
+    var jobs = $(this).siblings('.textarea').val();
+    var time = $(this).parent().attr('id');
+
+    localStorage.setItem(jobs, time);
+    console.log(jobs, time);
 })
 
+$('#hour7 .textarea').val(localStorage.getItem('hour7'));
+$('#hour8 .textarea').val(localStorage.getItem('hour8'));
+$('#hour9 .textarea').val(localStorage.getItem('hour9'));
+$('#hour10 .textarea').val(localStorage.getItem('hour10'));
+$('#hour11 .textarea').val(localStorage.getItem('hour11'));
+$('#hour12 .textarea').val(localStorage.getItem('hour12'));
+$('#hour13 .textarea').val(localStorage.getItem('hour13'));
+$('#hour14 .textarea').val(localStorage.getItem('hour14'));
+$('#hour15 .textarea').val(localStorage.getItem('hour15'));
+$('#hour16 .textarea').val(localStorage.getItem('hour16'));
+$('#hour17 .textarea').val(localStorage.getItem('hour17'));
+$('#hour18 .textarea').val(localStorage.getItem('hour18'));
+$('#hour19 .textarea').val(localStorage.getItem('hour19'));
+$('#hour20 .textarea').val(localStorage.getItem('hour20'));
+$('#hour21 .textarea').val(localStorage.getItem('hour21'));
+$('#hour22 .textarea').val(localStorage.getItem('hour22'));
 
-// Tryign to make the middle columns sortable 
+function timeChange() { 
+    $(".row").each(function (){
+        var timeBlock = parseInt($(this).attr("id").split('hour')[1]);
+        console.log(timeBlock, rightNow)
+    })
+}
 
 // $(".saveBtn").on("click", function()
 
